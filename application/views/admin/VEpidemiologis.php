8849b5b9be94bf4736421f37178f702f924c5902
Dedy Hermawan <48717251+DedyHrmawan@users.noreply.github.com>
2023-01-19 21:50:24 +0700

    kasih liat id pe

--- application/views/admin/VEpidemiologis.php
+++ application/views/admin/VEpidemiologis.php
@@ -15,11 +15,11 @@
                         <thead>
                             <tr class="fw-bolder text-muted">
                                 <th>No</th>
+                                <th>ID PE</th>
                                 <th>NIK</th>
                                 <th>Nama</th>
                                 <th>Kelurahan</th>
                                 <th>Tanggal PE</th>
-                                <th>Tanggal Mulai Sakit</th>
                                 <th class="min-w-150px">Aksi</th>
                             </tr>
                         </thead>
@@ -29,18 +29,16 @@
                             foreach ($list as $item) {
                                 $date = date_create($item->tgl_pe);
                                 $tglpe = date_format($date, "d F Y");
-                                $date = date_create($item->tgl_sakit);
-                                $tgl_sakit = date_format($date, "d F Y");
                                 $periksa = explode(";", $item->total_periksa);
                                 $pos = explode(";", $item->total_pos);
                                 echo '
                                     <tr>
                                         <td class="text-dark fw-bolder">' . $no . '</td>
+                                        <td class="text-dark fw-bolder">PE0' . $item->id_pe . '</td>
                                         <td class="text-dark fw-bolder">' . $item->nik_px . '</td>
                                         <td class="text-dark fw-bolder">' . $item->nama_px . '</td>
                                         <td class="text-dark fw-bolder">' . $item->nama_kelurahan . '</td>
                                         <td class="text-dark fw-bolder">' . $tglpe . '</td>
-                                        <td class="text-dark fw-bolder">' . $tgl_sakit . '</td>
                                         <td class="">
                                             <a href="" title="Detail PE" data-bs-toggle="modal" data-bs-target="#mdl_detPE" data-id="' . $item->id_pe . '" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm mdl_detPE me-1">
                                                 <span class="svg-icon svg-icon-muted svg-icon-2hx"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
